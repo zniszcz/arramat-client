4dc7a8a2368b749b1dba4c2fafa0686c9cc1ae1f
Adrian Zniszczyński <adrian@zniszczynski.pl>
2015-12-16 23:25:07 +0100

    Initialised yeomen angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arramatClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the arramatClientApp
+ */
+angular.module('arramatClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
